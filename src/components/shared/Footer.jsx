295d4425f143517a14425f3905c3ec224b7f24e9
Seba Flomenbaun <sebaflomen@gmail.com>
2025-12-02 09:58:18 -0300

    se cambio bg-color de navbar y footer transitorio, haciendo relacion a los colores elegidos por el grupo

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import '../../styles/footer.css';
 
 const Footer = () => {
     return (
-        <div>
+        <div className="footer-bg-color text-white py-3 mt-4">
             <p className='text-center'>Todos los derechos reservados</p>
         </div>
     );
--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -2,10 +2,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import '../../styles/menu.css';
 
 function Menu() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="navbar-bg-color">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
