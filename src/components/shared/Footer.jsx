a62d52d5d06ca91664d867dd12894368aa2ecfd9
Luis Robles <luisgeremiasrobles@gmail.com>
2025-12-03 14:30:11 -0300

    Instale la dependencia de bootstrap-icons y la importe en el componente main

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,10 +1,26 @@
 import '../../styles/footer.css';
+import { Link } from 'react-router';
 
 const Footer = () => {
     return (
-        <div className="footer-bg-color text-white py-3 mt-4">
-            <p className='text-center'>Todos los derechos reservados</p>
-        </div>
+        <footer className='container-fluid footer-bg-color text-white'>
+            <div className="row border border-light">
+                <div className="col-12 col-md-4 d-flex flex-column border border-light">
+                    <h4>Categorias</h4>
+                    <Link to={"/contacto"}>Contacto</Link>
+                    <Link>Quienes Somos</Link>
+                    <Link>Trabaja con nosotros</Link>
+                    <Link>Preguntas frecuentes</Link>
+                    <Link>Terminos y condiciones</Link>
+                </div>
+                <div className="col-12 col-md-4 border border-light">
+                    <h4>Contactanos</h4>
+                    <p className='text-white'><i className="bi bi-whatsapp"></i>+54 34566734560</p>
+                </div>
+                <div className="col-12 col-md-4 border border-light">1234</div>
+            </div>
+            <p className='text-center'>&copy; Copyright ViciAR 2025 Todos los derechos reservados</p>
+        </footer>
     );
 };
 
