3613d1b34467ed6d81875a2d0599c759fdd2b1f9
Eduardo Velazques <eduwavee@gmail.com>
2025-12-06 15:04:50 -0300

    Conflictos corregidos en app.jsx, package,json y package-lock.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import Menu from "./components/shared/Menu.jsx";
+import Footer from "./components/shared/Footer.jsx";
+import Admin from "./components/pages/Admin.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/pages/Home.jsx";
+import Inicio from "./components/pages/Inicio.jsx";
+import Carrito from "./components/pages/carrito/Carrito.jsx";
 import Register from "./components/pages/Register.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Menu />
+      <main>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/carrito" element={<Carrito />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/components/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "40px", fontSize: "32px" }}>
-      <h1>HOLA</h1>
-    </div>
-  );
-}
